7927f532c5d1768546b89efa01520838a24c2834
cr7joaofilho <joaovitorsilvafilho96@gmail.com>
2024-10-08 12:15:44 -0300

    241123123

--- peguntas.js
+++ peguntas.js
@@ -3,3 +3,99 @@ criaCartao(
     'como se calcula a soma dos termos de uma P.A',
     'Utiliza-se a fórmula: ((a1+an).q)//2'
 )
+criaCartao(
+    'historia', 
+    'quando foi a guerra fria ?',
+    '1947'
+)
+
+criaCartao(
+    'Português', 
+    'Qual a diferença de mas e mais?',
+    'Mais é utilizado para adição e mas para oposição'
+)
+criaCartao(
+    'quimica', 
+    'qual e a formula da agua ?',
+    'H2O'
+)
+criaCartao(
+    'Biologia', 
+    'Qual a chance de um descendente de um casal aaXaa apresentar a caracteristica do olho verde (ambos os pais tem olho verde)',
+    '100%'
+)
+criaCartao(
+    'quimica', 
+    'toda ligação entre ametais é covalente?',
+    'Sim'
+)
+criaCartao(
+    'Geografia', 
+    'Qual a capital da Alemanha',
+    'Berlim'
+)
+criaCartao(
+    'Sociologia', 
+    'Quem escreveu "O Capital"?',
+    'Karl Marx'
+)
+criaCartao(
+    'Matemática', 
+    'qual o resultado de 2²-1',
+    '3'
+)
+criaCartao(
+    'Física', 
+    'qual é a fórmula da velocidade?',
+    'Vm = ΔS/Δt.'
+)
+criaCartao(
+    'ingles ', 
+    'diga "olá" em inglês',
+    'Hello'
+)
+criaCartao(
+    'geografia', 
+    'quem foi o criador da clt?',
+    'Getúlio Vargas'
+)
+criaCartao(
+    'educacão física', 
+    'esporte mais jogado no mundo?',
+    'Futebol'
+)
+criaCartao(
+    'Biotecnologia', 
+    'Qual cor da biotecnologia abrange a medicina?',
+    'Vermelha'
+)
+criaCartao(
+    'robótica', 
+    'para que serve os jumpers?',
+    'é um condutor curto usado para fechar, abrir ou desviar parte de um circuito eletrônico'
+)
+criaCartao(
+    'Português', 
+    'Nome da palavra usada para dar características a algo ou alguém',
+    'Adjetivo'
+)
+criaCartao(
+    'inglês', 
+    'verbo usado para indicar o futuro',
+    'will'
+)
+criaCartao(
+    'física', 
+    'o que significa v na fisica?',
+    'volt'
+)
+criaCartao(
+    'inglês', 
+    'como perguntar "você está bem?"',
+    'How are you?'
+)
+criaCartao(
+    'Inglês', 
+    'Como se escreve Brasil em inglês?',
+    'Brazil'
+)
