4d19dade774160fec8aff4f2ff6e283a6990a163
cr7joaofilho <joaovitorsilvafilho96@gmail.com>
2024-10-15 11:38:41 -0300

    55555

--- main.js
+++ main.js
@@ -4,12 +4,12 @@ let cartao =document.createElement('article');
 cartao.className = 'cartao';
 cartao.innerHTML = `
 <div class="conteudocartao">
-    <h3>categoria</h3>
+    <h3>${categoria}</h3>
     <div class="perguntacartao">
-        <p>pergunta </p>
+        <p>${pergunta} </p>
     </div>
     <div class="respostacartao">
-        <p>resposta</p>
+        <p>${resposta}</p>
     </div>
 </div>
 ` 
